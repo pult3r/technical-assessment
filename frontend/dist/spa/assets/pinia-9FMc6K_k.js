3342b04c19a93dad47df5d3459eac4021dcc8f8b
pult3r <lukasz.pulter@gmail.com>
2026-02-08 20:55:54 +0100

    build: production build for Cloudways deploy

---
+++ frontend/dist/spa/assets/pinia-9FMc6K_k.js
@@ -0,0 +1,1 @@
+import{b as o,c as t}from"./index-Bbv3GLJq.js";const e=o(({app:a})=>{const i=t();a.use(i)});export{e as default};
---
+++ frontend/dist/spa/assets/router-guard-fs2Ipapv.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-Bbv3GLJq.js";const t=o(()=>{});export{t as default};
